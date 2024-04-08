155ea2d627304ff0d5e409c73a290a9312504b17
SougataMondal2000 <sougataofficial362000@gmail.com>
2024-04-09 02:49:21 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,110 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import { SiChatbot } from "react-icons/si";
+import { RxCross2 } from "react-icons/rx";
+import { IoSendSharp } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [dateTime, setDateTime] = useState(new Date());
+  const [chat, setChat] = useState([
+    { msg: "Hi! How can I help you?", isBotChat: true },
+  ]);
+
+  useEffect(() => {
+    if (open) {
+      setDateTime(new Date());
+    }
+  }, [open]);
+
+  const sendMessage = () => {
+    if (message.trim() !== "") {
+      const newUserMessage = { msg: message, isBotChat: false };
+      const newBotMessage = {
+        msg: "Sorry, I'm not quite ready! Why don't you browse some properties for now?",
+        isBotChat: true,
+      };
+      setChat([...chat, newUserMessage, newBotMessage]);
+      setMessage("");
+      setDateTime(new Date());
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div
+        className="bg-[#006d77] w-fit h-fit rounded-full fixed bottom-0 right-0 m-10 cursor-pointer p-1 hover:p-2"
+        onClick={() => setOpen(!open)}
+      >
+        {open ? (
+          <RxCross2 className="w-12 h-12 text-white p-2" />
+        ) : (
+          <SiChatbot className="w-12 h-12 text-white p-2" />
+        )}
+      </div>
+      {open && (
+        <div className="fixed bottom-28 right-12 md:h-[510px] md:w-[350px] border border-gray-100 shadow-md rounded-lg">
+          <div className="">
+            <div>
+              <div className="bg-[#006d77] flex justify-start items-center gap-2 w-full p-2 text-white rounded-t-lg">
+                <SiChatbot className="w-12 h-12  p-2" />
+                <p className="text-xl font-bold">BuilderFloor Chat</p>
+              </div>
+              <div className=" h-[355px] overflow-y-scroll">
+                {chat.map((item, index) => (
+                  <div
+                    className={`flex gap-2 m-2 mb-4 ${
+                      item.isBotChat ? "justify-start" : "justify-end"
+                    }`}
+                    key={index}
+                  >
+                    {item.isBotChat && (
+                      <SiChatbot className="w-5 h-5 text-[#006d77] " />
+                    )}
+                    <div>
+                      <p
+                        className={`text-xs mb-1 ${
+                          item.isBotChat ? "text-left" : "text-right"
+                        }`}
+                      >
+                        {dateTime.toLocaleDateString()}
+                        {", "}
+                        {dateTime.toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </p>
+                      <p className="bg-[#e5f0f1] rounded-sm py-1 px-3 text-wrap max-w-56">
+                        {item.msg}
+                      </p>
+                    </div>
+                    {!item.isBotChat && (
+                      <FaUser className="w-5 h-5 text-[#006d77] " />
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="">
+              <div className="m-1 flex items-end justify-between">
+                <textarea
+                  placeholder="Write your query..."
+                  className="w-[300px] outline-none bg-[#e5f0f1] rounded-md py-1 px-2"
+                  rows={3}
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+                <IoSendSharp
+                  className="w-6 h-6 text-[#006d77] cursor-pointer"
+                  onClick={sendMessage}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
   );
 }
