ba9de3eaf443f35eb5483496598fee54bfb58a3c
SougataMondal2000 <sougataofficial362000@gmail.com>
2024-04-09 16:37:47 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import { SiChatbot } from "/node_modules/react-icons/si";
 import { RxCross2 } from "/node_modules/react-icons/rx";
 import { IoSendSharp } from "/node_modules/react-icons/io5";
@@ -9,9 +10,8 @@ export default function Home() {
   const [open, setOpen] = useState(false);
   const [message, setMessage] = useState("");
   const [dateTime, setDateTime] = useState(new Date());
-  const [chat, setChat] = useState([
-    { msg: "Hi! How can I help you?", isBotChat: true },
-  ]);
+  const [chat, setChat] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (open) {
@@ -20,20 +20,33 @@ export default function Home() {
   }, [open]);
 
   const sendMessage = () => {
-    if (message.trim() !== "") {
-      const newUserMessage = { msg: message, isBotChat: false };
-      const newBotMessage = {
-        msg: "Sorry, I'm not quite ready! Why don't you browse some properties for now?",
-        isBotChat: true,
-      };
-      setChat([...chat, newUserMessage, newBotMessage]);
-      setMessage("");
-      setDateTime(new Date());
+    const payload = { query: message };
+    axios
+      .post("https://chatbot.trainright.fit/api/chat", payload)
+      .then((res) => setChat(res.data), setMessage(""), setDateTime(new Date()))
+      .catch((err) => console.log(err, "Error!"))
+      .finally(() => setLoading(false));
+  };
+
+  const chatContainerRef = useRef(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chat]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
     }
   };
 
   return (
-    <main>
+    <div>
       <div
         className="bg-[#006d77] w-fit h-fit rounded-full fixed bottom-0 right-0 m-10 cursor-pointer p-1 hover:p-2"
         onClick={() => setOpen(!open)}
@@ -45,28 +58,42 @@ export default function Home() {
         )}
       </div>
       {open && (
-        <div className="fixed bottom-28 right-12 md:h-[510px] md:w-[350px] border border-gray-100 shadow-md rounded-lg">
+        <div className="bg-white fixed bottom-28 right-12 md:h-[510px] md:w-[350px] border border-gray-100 shadow-md rounded-lg">
           <div className="">
             <div>
               <div className="bg-[#006d77] flex justify-start items-center gap-2 w-full p-2 text-white rounded-t-lg">
                 <SiChatbot className="w-12 h-12  p-2" />
                 <p className="text-xl font-bold">BuilderFloor Chat</p>
               </div>
-              <div className=" h-[355px] overflow-y-scroll">
-                {chat.map((item, index) => (
+              <div
+                className=" h-[355px] overflow-y-scroll"
+                ref={chatContainerRef}
+                style={{ scrollBehavior: "smooth" }}
+              >
+                <p className="bg-[#e5f0f1] text-center rounded-sm py-1 px-3 m-2">
+                  Hi, ask your queries here!
+                </p>
+                {/* {loading && (
+                  <p className="bg-[#e5f0f1] text-center rounded-sm py-1 px-3 m-2">
+                    Loading...
+                  </p>
+                )} */}
+                {chat?.map((item, index) => (
                   <div
                     className={`flex gap-2 m-2 mb-4 ${
-                      item.isBotChat ? "justify-start" : "justify-end"
+                      item.role === "assistant"
+                        ? "justify-start"
+                        : "justify-end"
                     }`}
                     key={index}
                   >
-                    {item.isBotChat && (
+                    {item.role === "assistant" && (
                       <SiChatbot className="w-5 h-5 text-[#006d77] " />
                     )}
                     <div>
                       <p
                         className={`text-xs mb-1 ${
-                          item.isBotChat ? "text-left" : "text-right"
+                          item.role === "assistant" ? "text-left" : "text-right"
                         }`}
                       >
                         {dateTime.toLocaleDateString()}
@@ -77,10 +104,10 @@ export default function Home() {
                         })}
                       </p>
                       <p className="bg-[#e5f0f1] rounded-sm py-1 px-3 text-wrap max-w-56">
-                        {item.msg}
+                        {item.content}
                       </p>
                     </div>
-                    {!item.isBotChat && (
+                    {item.role === "user" && (
                       <FaUser className="w-5 h-5 text-[#006d77] " />
                     )}
                   </div>
@@ -105,6 +132,6 @@ export default function Home() {
           </div>
         </div>
       )}
-    </main>
+    </div>
   );
 }
